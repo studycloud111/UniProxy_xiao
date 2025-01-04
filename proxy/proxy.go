0ce33d4cd06073962afe31288b344c26a00f7f53
studycloud111 <74172237+studycloud111@users.noreply.github.com>
2025-01-04 20:26:03 +0800

    Update proxy.go

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,62 +1,84 @@
 package proxy
 
 import (
-	"context"
+    "context"
+    "net/netip"  // 添加这个导入
 
-	box "github.com/sagernet/sing-box"
-	"github.com/studycloud111/UniProxy_xiao/common/sysproxy"
-	"github.com/studycloud111/UniProxy_xiao/v2b"
+    box "github.com/sagernet/sing-box"
+    "github.com/sagernet/sing-box/option"  // 添加这个导入
+    "github.com/sagernet/sing/service"     // 添加这个导入
+    "github.com/studycloud111/UniProxy_xiao/common/sysproxy"
+    "github.com/studycloud111/UniProxy_xiao/v2b"
 )
 
 var (
-	Running     bool
-	SystemProxy bool
-	GlobalMode  bool
-	TunMode     bool
-	InPort      int
-	DataPath    string
-	ResUrl      string
+    Running     bool
+    SystemProxy bool
+    GlobalMode  bool
+    TunMode     bool
+    InPort      int
+    DataPath    string
+    ResUrl      string
 )
 
 var client *box.Box
 
 func StartProxy(tag string, uuid string, server *v2b.ServerInfo) error {
-	if Running {
-		StopProxy()
-	}
-	SystemProxy = true
-	c, err := GetSingBoxConfig(uuid, server)
-	if err != nil {
-		return err
-	}
-	client, err = box.New(box.Options{
-		Context: context.Background(),
-		Options: c,
-	})
-	if err != nil {
-		return err
-	}
-	err = client.Start()
-	if err != nil {
-		return err
-	}
-	Running = true
-	return nil
+    if Running {
+        StopProxy()
+    }
+    SystemProxy = true
+    c, err := GetSingBoxConfig(uuid, server)
+    if err != nil {
+        return err
+    }
+
+    // 创建带 registry 的 context
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    // 创建一个新的 registry
+    registry := service.NewRegistry(ctx)
+
+    // 将 registry 添加到 context
+    ctx = service.ContextWithRegistry(ctx, registry)
+
+    // 创建 box 实例
+    client, err = box.New(box.Options{
+        Context: ctx,
+        Options: c,
+    })
+    if err != nil {
+        return err
+    }
+
+    err = client.Start()
+    if err != nil {
+        return err
+    }
+
+    Running = true
+    return nil
 }
 
 func StopProxy() {
-	if Running {
-		client.Close()
-		Running = false
-	}
+    if Running {
+        if client != nil {
+            client.Close()
+            client = nil
+        }
+        Running = false
+    }
 }
 
 func ClearSystemProxy() error {
-	if Running {
-		client.Close()
-		Running = false
-		return nil
-	}
-	sysproxy.ClearSystemProxy()
-	return nil
+    if Running {
+        if client != nil {
+            client.Close()
+            client = nil
+        }
+        Running = false
+    }
+    sysproxy.ClearSystemProxy()
+    return nil
 }
